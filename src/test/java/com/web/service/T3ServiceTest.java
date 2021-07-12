06e51e7a0407f73cd95133807286e34631fd7eea
lordchavez <lordchavez@gmail.com>
2021-07-12 14:40:43 -0500

    Subir

--- src/test/java/com/web/service/T3ServiceTest.java
+++ src/test/java/com/web/service/T3ServiceTest.java
@@ -1,16 +1,29 @@
 package com.web.service;
 
+import com.web.entity.T1;
+import com.web.entity.T3;
+import com.web.repository.T3Repository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class T3ServiceTest {
 
-    
+    @InjectMocks
+    private T3ServiceImpl service;
+
+    @Mock
+    private T3Repository repository;
 
     @Test
     public void testFindById() {
-
+        when( repository.findById( 1 ) ).thenReturn( new T3( 1, new T1(), "" ) );
+        Assert.assertNotNull( service.findById( 1 ) );
     }
 }
